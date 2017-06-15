23c34cb0ef1e4e7681cc587ebd2d659063a4af4c
nagayasu-shinya <nagayasu_shinya@yahoo.co.jp>
2017-06-15 15:16:27 +0900

    modify comment.

--- kernel/wait.h
+++ kernel/wait.h
@@ -1,12 +1,15 @@
+#ifndef _WAIT_H_
+#define _WAIT_H_
+
 /*
  *  TOPPERS/JSP Kernel
  *      Toyohashi Open Platform for Embedded Real-Time Systems/
  *      Just Standard Profile Kernel
- * 
+ *
  *  Copyright (C) 2000 by Embedded and Real-Time Systems Laboratory
  *                              Toyohashi Univ. of Technology, JAPAN
- * 
- *  上記著作権者は，以下の (1)〜(4) の条件か，Free Software Foundation 
+ *
+ *  上記著作権者は，以下の (1)〜(4) の条件か，Free Software Foundation
  *  によって公表されている GNU General Public License の Version 2 に記
  *  述されている条件を満たす場合に限り，本ソフトウェア（本ソフトウェア
  *  を改変したものを含む．以下同じ）を使用・複製・改変・再配布（以下，
@@ -27,21 +30,22 @@
  *        報告すること．
  *  (4) 本ソフトウェアの利用により直接的または間接的に生じるいかなる損
  *      害からも，上記著作権者およびTOPPERSプロジェクトを免責すること．
- * 
+ *
  *  本ソフトウェアは，無保証で提供されているものである．上記著作権者お
  *  よびTOPPERSプロジェクトは，本ソフトウェアに関して，その適用可能性も
  *  含めて，いかなる保証も行わない．また，本ソフトウェアの利用により直
  *  接的または間接的に生じたいかなる損害に関しても，その責任を負わない．
- * 
+ *
  *  @(#) $Id: wait.h,v 1.6 2003/06/04 01:46:16 hiro Exp $
  */
 
-/*
- *	待ち状態管理モジュール
- */
 
-#ifndef _WAIT_H_
-#define _WAIT_H_
+/**
+ * @file
+ * @brief 待ち状態管理モジュール
+ *
+ * @copyright 2000 by Embedded and Real-Time Systems Laboratory Toyohashi Univ. of Technology, JAPAN
+ */
 
 #include "task.h"
 #include "time_event.h"
@@ -56,9 +60,9 @@
 Inline void
 make_wait(WINFO *winfo)
 {
-	make_non_runnable(runtsk);
-	runtsk->winfo = winfo;
-	winfo->tmevtb = NULL;
+    make_non_runnable(runtsk);
+    runtsk->winfo = winfo;
+    winfo->tmevtb = NULL;
 }
 
 /*
@@ -69,7 +73,7 @@ make_wait(WINFO *winfo)
  *  ルド，WINFO の tmevtbフィールドを設定する．また，タイムイベントブ
  *  ロックを登録する．
  */
-extern void	make_wait_tmout(WINFO *winfo, TMEVTB *tmevtb, TMO tmout);
+extern void make_wait_tmout(WINFO *winfo, TMEVTB *tmevtb, TMO tmout);
 
 /*
  *  待ち解除
@@ -80,7 +84,7 @@ extern void	make_wait_tmout(WINFO *winfo, TMEVTB *tmevtb, TMO tmout);
  *  からの削除は行わない．待ち解除したタスクへのディスパッチが必要な場
  *  合には TRUE を返す．
  */
-extern BOOL	wait_complete(TCB *tcb);
+extern BOOL wait_complete(TCB *tcb);
 
 /*
  *  タイムアウトに伴う待ち解除
@@ -94,8 +98,8 @@ extern BOOL	wait_complete(TCB *tcb);
  *  いずれの関数も，タイムイベントのコールバック関数として用いるための
  *  もので，割込みハンドラから呼び出されることを想定している．
  */
-extern void	wait_tmout(TCB *tcb);
-extern void	wait_tmout_ok(TCB *tcb);
+extern void wait_tmout(TCB *tcb);
+extern void wait_tmout_ok(TCB *tcb);
 
 /*
  *  待ち状態の強制解除
@@ -108,8 +112,8 @@ extern void	wait_tmout_ok(TCB *tcb);
  *  値を E_RLWAI とする．また，待ち解除したタスクへのディスパッチが必
  *  要な場合には TRUE を返す．
  */
-extern void	wait_cancel(TCB *tcb);
-extern BOOL	wait_release(TCB *tcb);
+extern void wait_cancel(TCB *tcb);
+extern BOOL wait_release(TCB *tcb);
 
 /*
  *  同期・通信オブジェクトの管理ブロックの共通部分操作ルーチン
@@ -127,35 +131,34 @@ extern BOOL	wait_release(TCB *tcb);
  *  同期・通信オブジェクトの初期化ブロックの共通部分
  */
 typedef struct wait_object_initialization_block {
-	ATR	wobjatr;	/* オブジェクト属性 */
+    ATR    wobjatr;    /* オブジェクト属性 */
 } WOBJINIB;
 
 /*
  *  同期・通信オブジェクトの管理ブロックの共通部分
  */
 typedef struct wait_object_control_block {
-	QUEUE		wait_queue;	/* 待ちキュー */
-	const WOBJINIB	*wobjinib;	/* 初期化ブロックへのポインタ */
+    QUEUE           wait_queue;    /* 待ちキュー */
+    const WOBJINIB *wobjinib;      /* 初期化ブロックへのポインタ */
 } WOBJCB;
 
 /*
  *  同期・通信オブジェクト待ち情報ブロックの定義
  */
 typedef struct wait_object_waiting_information {
-	WINFO	winfo;		/* 標準の待ち情報ブロック */
-	WOBJCB	*wobjcb;	/* 待ちオブジェクトの管理ブロック */
+    WINFO    winfo;     /* 標準の待ち情報ブロック */
+    WOBJCB  *wobjcb;    /* 待ちオブジェクトの管理ブロック */
 } WINFO_WOBJ;
 
 /*
  *  同期・通信オブジェクトに対する待ち状態への移行
- *  
+ *
  *  実行中のタスクを待ち状態に移行させ，同期・通信オブジェクトの待ちキュー
  *  につなぐ．また，待ち情報ブロック（WINFO）の wobjcb を設定する．
  *  wobj_make_wait_tmout は，タイムイベントブロックの登録も行う．
  */
-extern void	wobj_make_wait(WOBJCB *wobjcb, WINFO_WOBJ *winfo);
-extern void	wobj_make_wait_tmout(WOBJCB *wobjcb, WINFO_WOBJ *winfo,
-					TMEVTB *tmevtb, TMO tmout);
+extern void wobj_make_wait(WOBJCB *wobjcb, WINFO_WOBJ *winfo);
+extern void wobj_make_wait_tmout(WOBJCB *wobjcb, WINFO_WOBJ *winfo, TMEVTB *tmevtb, TMO tmout);
 
 /*
  *  タスク優先度変更時の処理
@@ -163,6 +166,6 @@ extern void	wobj_make_wait_tmout(WOBJCB *wobjcb, WINFO_WOBJ *winfo,
  *  同期・通信オブジェクトに対する待ち状態にあるタスクの優先度が変更さ
  *  れた場合に，待ちキューの中でのタスクの位置を修正する．
  */
-extern void	wobj_change_priority(WOBJCB *wobjcb, TCB *tcb);
+extern void wobj_change_priority(WOBJCB *wobjcb, TCB *tcb);
 
 #endif /* _WAIT_H_ */
